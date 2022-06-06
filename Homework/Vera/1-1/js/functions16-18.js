b9888dca3d44befdcbf37d8e3ca751452fc69119
JudsonH <judson.hatfield.korea@gmail.com>
2022-06-07 00:23:12 +0900

    update cover url

--- Homework/Vera/1-1/js/functions16-18.js
+++ Homework/Vera/1-1/js/functions16-18.js
@@ -69,14 +69,14 @@ Amplitude.init({
             artist: "Vera the Alien Hunter",
             album: "Vera's Tall Tales: Pages 16-18",
             url: "mp3s/Vera 1-1 Listen to the Story 16-18.mp3",
-            cover_art_url: "images/1-1 Cover.png",
+            cover_art_url: "images/Cover.png",
         },
         {
             name: "Listen and Repeat",
             artist: "Vera the Alien Hunter",
             album: "Vera's Tall Tales: Pages 16-18",
             url: "mp3s/Vera 1-1 Listen and Repeat 16-18.mp3",
-            cover_art_url: "images/1-1 Cover.png",
+            cover_art_url: "images/Cover.png",
         },
     ],
     callbacks: {
